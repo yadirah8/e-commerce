9d20c00c0641918a62daca9539e51dd4862b9fec
yadirah8 <yadinwandu4@gmail.com>
2021-12-22 21:43:02 +0100

    feat: add eslint function

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express =  require("express");
+const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send("Hello, World!!!");
+  return res.send('Hello, World!!!');
 });
 
 app.listen( 3000, () => {
-   console.log("Listening at port 3000...");
+  console.log('Listening at port 3000...');
 });
