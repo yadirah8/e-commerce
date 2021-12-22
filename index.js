f276e93f9372b3aa859b1d9a68793880c81b5056
yadirah8 <yadinwandu4@gmail.com>
2021-12-22 20:47:08 +0100

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express =  require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send("Hello World!!!");
+});
+
+app.listen( 3000, () => {
+   console.log("Listening at port 3000...");
+});
